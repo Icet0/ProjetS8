6ad4568ca58635b12f9b77dc4ec3be7e2381ea28
Icet0 <49767388+Icet0@users.noreply.github.com>
2022-04-15 19:01:42 +0200

    message service

--- NewProject/Frontend/LogAnalyse/src/app/app.module.ts
+++ NewProject/Frontend/LogAnalyse/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { VisualisationComponent } from './visualisation/visualisation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    VisualisationComponent
   ],
   imports: [
     BrowserModule,
