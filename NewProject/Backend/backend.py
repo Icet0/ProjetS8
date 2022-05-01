4a37c20657e33956513577c942bf340f23b29cac
Icet0 <49767388+Icet0@users.noreply.github.com>
2022-05-01 17:01:07 +0200

    getalltab+affluence

--- NewProject/Backend/backend.py
+++ NewProject/Backend/backend.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template,request,jsonify,redirect, url_for
+from flask import Flask,render_template,request,jsonify,redirect, url_for,Response,stream_with_context
+
 import json
 from flask import jsonify
 import pandas as pd
+from flask import make_response
+import gzip
 
 
 def create_app():
@@ -11,6 +14,7 @@ def create_app():
 app = create_app()
 
 
+
 @app.route("/")
 def hello():
     return 'Hello, World!'
@@ -18,7 +22,7 @@ def hello():
 
 
 @app.route("/json",methods=['POST','GET'])
-def json():
+def visualisation():
       try:
         myUrl = request.environ['HTTP_ORIGIN']
       except KeyError:
@@ -26,7 +30,7 @@ def json():
         myUrl = '*'
       
       # data = [{"lol":1,"cocorico":"ZARBI"},{"lol":2,"cocorico":"WTF"}] #exemple de la forme de donnée à retourner
-      data = getallTAB().to_dict(orient = 'records')
+      data = getTAB().to_dict(orient = 'records')
       de = {"status":"OK",
              "data":data}
       response = jsonify(de)
@@ -36,6 +40,127 @@ def json():
       response.headers.add('Content-type','application/json')
       response.headers.add('charset','utf8')
       return response
+    
+
+@app.route("/tryAll",methods=['POST','GET'])
+def tryAll():
+      try:
+        myUrl = request.environ['HTTP_ORIGIN']
+      except KeyError:
+        print("KeyError lol")
+        myUrl = '*'
+      
+      data = getallTAB() #get all tab
+      data = data.head(10000)
+      data = data.to_dict(orient = 'records')
+      
+      de = {"status":"OK",
+             "data":data}
+    
+
+      content = gzip.compress(json.dumps(de).encode('utf8'), 5)
+      response = make_response(content)
+      response.headers.add('Access-Control-Allow-Origin',myUrl)
+      response.headers.add('Access-Control-Allow-Credentials', 'true')
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+      response.headers.add('Content-type','application/json')
+      response.headers.add('charset','utf8')
+      # response.headers['Content-length'] = len(content)
+      response.headers['Content-Encoding'] = 'gzip'
+      return response
+
+
+
+    
+@app.route("/tryAll2",methods=['POST','GET'])
+def tryAll2():
+      try:
+        myUrl = request.environ['HTTP_ORIGIN']
+      except KeyError:
+        print("KeyError lol")
+        myUrl = '*'
+      
+      data = getallTAB() #get all tab
+      taille = data.shape[0]
+      data = data.to_dict(orient = 'records')
+      de = {"status":"OK",
+             "data":data}
+    
+      response = de
+      @stream_with_context
+      def generate():       
+          print(taille)
+          for i in range(taille):
+              yield json.dumps(response["data"][i])
+
+      # response = make_response(response)
+      # response.headers.add('Access-Control-Allow-Origin',myUrl)
+      # response.headers.add('Access-Control-Allow-Credentials', 'true')
+      # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+      # response.headers.add('Content-type','application/json')
+      # response.headers.add('charset','utf8')
+      return Response(generate(), content_type='application/json')
+
+
+
+
+
+@app.route("/afluence",methods=['POST'])
+def afluence():
+      try:
+        myUrl = request.environ['HTTP_ORIGIN']
+      except KeyError:
+        print("KeyError lol")
+        myUrl = '*'
+      
+      # data = [{"lol":1,"cocorico":"ZARBI"},{"lol":2,"cocorico":"WTF"}] #exemple de la forme de donnée à retourner
+      data = getTAB().to_dict(orient = 'records')
+      de = {"status":"OK",
+             "data":data}
+      response = jsonify(de)
+      response.headers.add('Access-Control-Allow-Origin',myUrl)
+      response.headers.add('Access-Control-Allow-Credentials', 'true')
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+      response.headers.add('Content-type','application/json')
+      response.headers.add('charset','utf8')
+      return response
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/test")
+def test():
+  data = getTAB()
+  taille = data.shape[0]
+  data = data.to_dict(orient = 'records')
+  de = {"status":"OK",
+          "data":data}
+  de["data"][0]["StatusCode"] = taille
+  response = jsonify(de["data"][0])
+  return response
+
+
+    
+@app.route('/stream')
+def streamed_response():
+    stre = "abcdefgijklmnopqrstuvwxyz"
+    @stream_with_context
+    def generate():
+        for i in range (1000):
+          yield stre[i%len(stre)]
+          yield "\n"
+    return Response((generate()))
+ 
 
 
 
@@ -44,7 +169,7 @@ def getTAB():
     url = "./logs/dataset.txt"
     full_df2=pd.read_csv(url, sep=" ", encoding="utf-8")
     full_df2.columns =['Date', 'Heure', 'ConsultedPage', 'IP','VisitedSite', 'StatusCode','DataBytes']
-    df2 = full_df2.head(10)
+    df2 = full_df2.head(1000)
     return df2
 
 def getallTAB():
@@ -52,6 +177,9 @@ def getallTAB():
     full_df2=pd.read_csv(url, sep=" ", encoding="utf-8")
     full_df2.columns =['Date', 'Heure', 'ConsultedPage', 'IP','VisitedSite', 'StatusCode','DataBytes']
     return full_df2
+
+
+
 
 
 
--- NewProject/Frontend/LogAnalyse/src/app/message/message.service.ts
+++ NewProject/Frontend/LogAnalyse/src/app/message/message.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {DataFormat} from "./DataFormat";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
@@ -12,6 +12,8 @@ export class MessageService {
   constructor(private service : HttpClient) { }
 
   sendMessage(Url : string, data : any): Observable<DataFormat>{
+
+    // const headers =  new HttpHeaders()
     let realUrl = environment.backend+Url;
     console.log(realUrl);
     let retour : Observable<DataFormat> = new Observable<DataFormat>();
@@ -22,7 +24,7 @@ export class MessageService {
         console.log("key = "+key+" values = "+data[key]);
       }
     }
-    retour = this.service.post<DataFormat>(realUrl,formData,{withCredentials:true});
+    retour = this.service.post<DataFormat>(realUrl,formData,{withCredentials:true, });
     return retour;
   }
 }
