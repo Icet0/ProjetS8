0ece79082b88342daeabcff4d6a1aef6a5ffbb06
Bilal <79008357+bilalsquad@users.noreply.github.com>
2022-05-14 11:47:22 +0200

    Debut Création Carte

--- NewProject/Frontend/LogAnalyse/src/app/message/message.service.ts
+++ NewProject/Frontend/LogAnalyse/src/app/message/message.service.ts
@@ -27,4 +27,8 @@ export class MessageService {
     retour = this.service.post<DataFormat>(realUrl,formData,{withCredentials:true });
     return retour;
   }
+
+  getmessage(url:string) : Observable<any>{
+    return this.service.get<any>(url) ;
+  }
 }
