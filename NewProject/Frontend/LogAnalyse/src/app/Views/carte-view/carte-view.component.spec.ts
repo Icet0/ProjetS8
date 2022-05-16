90360473db00dd3db08ba7f83eb496ca524e8b77
Bilal <79008357+bilalsquad@users.noreply.github.com>
2022-05-16 08:27:45 +0200

    Debut Création Carte

---
+++ NewProject/Frontend/LogAnalyse/src/app/Views/carte-view/carte-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarteViewComponent } from './carte-view.component';
+
+describe('CarteViewComponent', () => {
+  let component: CarteViewComponent;
+  let fixture: ComponentFixture<CarteViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarteViewComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarteViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
