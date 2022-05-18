b68ba0080079e95ffe4af29c3c8310d22f78162e
livannm <65709107+livannm@users.noreply.github.com>
2022-05-18 22:01:10 +0200

    ajout submitLogin

--- NewProject/Frontend/LogAnalyse/src/app/login/login.component.ts
+++ NewProject/Frontend/LogAnalyse/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {MessageService} from "../message/message.service";
 
 @Component({
   selector: 'app-login',
@@ -8,19 +9,41 @@ import {Router} from "@angular/router";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private rooter:Router) { }
+  login= "" ;
+  password="";
+  errorMessage="";
+  constructor(private http:HttpClient,private message:MessageService,private router:Router) { }
 
   ngOnInit(): void {
   }
 
   submitRegister(){
     console.log("in submitRegister ");
-    this.rooter.navigateByUrl("/register");
+    this.router.navigateByUrl("/register");
   }
 
-  submitLogin(){
 
+  submitLogin():void{
+    if(this.login==""){
+      this.errorMessage= " Veuillez saisir votre login ";
+    }
+    else if(this.password == ""){
+      this.errorMessage = " Veuillez saisir votre mot de passe ";
+    }
+    else{
+      this.errorMessage = "";
+    }
+    let tmp = {username:this.login,password:this.password};
+    this.message.sendMessage("/authentification",tmp).subscribe(
+      (phpData)=>{
+        if(phpData.status == 'OK'){
+          console.log('données : '+phpData.data['login']);
+          this.router.navigateByUrl('/');
+        }
+        else {
+          console.log('données : '+phpData.data['reason']);
+        }
+      })
   }
 
 }
