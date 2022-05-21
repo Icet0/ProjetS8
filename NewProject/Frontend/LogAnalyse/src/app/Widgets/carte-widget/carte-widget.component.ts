80db747e2e84bf374f6c17ccc95db486232eb054
Bilal <79008357+bilalsquad@users.noreply.github.com>
2022-05-21 22:25:56 +0200

    Back Réparer et recuperation des donnée

--- NewProject/Frontend/LogAnalyse/src/app/Widgets/carte-widget/carte-widget.component.ts
+++ NewProject/Frontend/LogAnalyse/src/app/Widgets/carte-widget/carte-widget.component.ts
@@ -122,7 +122,43 @@ export class CarteWidgetComponent implements OnInit,ImplRecherche {
   public afficheMarker()
   {
 
+      const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
+      });
+
+       const svgMarker = {
+    path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "blue",
+    fillOpacity: 0.6,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2,
+    anchor: new google.maps.Point(15, 30),
+  };
+
+            let label = "Adresse IP :"+this.ip +'<br>'+"nombre de visite: "+this.nb_occur;
+
+          // Create markers.
+          for (let element of Object.entries(this.ipList)) {
+
+            const marker = new google.maps.Marker({
+              position:({lat : element[1][1].lat , lng :element[1][1].lon }),
+              icon: svgMarker,
+              map: this.map,
+            });
+
+
+         marker.addListener("click", () => {
+          infoWindow.setContent(label);
+          infoWindow.open(this.map!, marker);
+        });
+
+          }
+
   }
+
+
 
 
 
@@ -164,8 +200,6 @@ export class CarteWidgetComponent implements OnInit,ImplRecherche {
           //exemple getIpSite()
 
           //-------------------------------------------------
-
-
          this.recupererIPSite(elem)
         }
       }
